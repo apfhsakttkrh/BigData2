39a84df209c905c9d518a95de0785bcef6796142
apfhsakttkrh <apfhsakttkrh@naver.com>
2024-09-09 12:36:11 +0900

    v0.55 BP2

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -7,11 +7,10 @@ import pandas as pd
 #      }, index = [1, 2, 3]
 # )
 
-df = pd.DataFrame(
-    [[4, 5, 6],
+data =   [[4, 5, 6],
          [7, 8, 9],
          [10, 11, 12]],
-    index = [1, 2, 3], columns=["a","b","c"])
+df = pd.DataFrame(data, index = [1, 2, 3], columns=["a","b","c"])
 
 
-print(df)
+print(df['b'].nunique())
