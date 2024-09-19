78742a459085339eaa9979fa9295eedb42412700
apfhsakttkrh <apfhsakttkrh@naver.com>
2024-09-19 10:24:49 +0900

    v.9 BP2

--- week04/week04.py
+++ week04/week04.py
@@ -6,6 +6,7 @@ api = "https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp"
 station_id = input("지역코드 : ") #전국 108, 수도권109, 강원 105, 제주 184 ...
 values =  {'stnId':station_id}
 
-url = api +'?'+str(values)
+url = api +'?'+urllib.parse.urlencode(values)
+
 print(url)
 
