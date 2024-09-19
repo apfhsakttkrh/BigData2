14a78250f94ee0f146ac6d5e211cc4da763b2c5a
apfhsakttkrh <apfhsakttkrh@naver.com>
2024-09-19 12:49:05 +0900

    v1.3 BP2

--- week05/week05_np.py
+++ week05/week05_np.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 
 # Download and prepare the data
 lifesat = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
 X = lifesat[["GDP per capita (USD)"]].values
 y = lifesat[["Life satisfaction"]].values
 
-print(lifesat.head(27))
+#print(lifesat.head(27))
 
 # Visualize the data
 lifesat.plot(kind='scatter', grid=True,
@@ -17,7 +18,8 @@ plt.axis([23_500, 62_500, 4, 9])
 plt.show()
 
 # Select a linear model
-model = LinearRegression()  # 선형 회귀 모델 적용
+#model = LinearRegression()  # 선형 회귀 모델 적용
+model = KNeighborsRegressor(n_neighbors=3)  # 한국과 1인당 gdp가 가장 가까운 세 나라
 
 # Train the model
 model.fit(X, y)
